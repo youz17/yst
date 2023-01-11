7007974e4c87612bba0df3d6eaa38b5f3e10fa4d
youz <youz17@qq.com>
2023-01-11 23:50:01 +0800

    update test_hack

--- tests/test_hack.py
+++ tests/test_hack.py
@@ -1,6 +1,8 @@
 import yst.hack
+from yst.fp import *
+from operator import add
 
 l = [1, 2, 3]
 assert l.size(), 3
 
-assert list(l.map(lambda x: x + 1)), [2, 3, 4]
+assert l.map(bind(add, 1)).to_list(), [2, 3, 4]
